984265c5ae493b4550c896b802a4185a367eba1c
Nino Müller <nino.mueller@gmail.com>
2020-10-29 18:29:25 +0100

    Switched to RESTSharp

--- Bexio.cs
+++ Bexio.cs
@@ -3,47 +3,62 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using bexio.net.Models;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using bexio.net.Models;
+using RestSharp;
 
 namespace bexio.net
 {
     public class Bexio
     {
-        public string _url;
-        public string _apiToken;
+        private readonly string _url;
+        private readonly string _apiToken;
         public HttpClient _httpClient = new HttpClient();
+
+        private readonly RestClient _restClient;
 
         public Bexio(string url, string apiToken) {
             _url = url;
             _apiToken = apiToken;
 
-            _httpClient.DefaultRequestHeaders
-                .Accept
-                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _restClient = new RestClient();
 
-            _httpClient.BaseAddress = new Uri(url);
-
-            _httpClient.DefaultRequestHeaders.Authorization = GenerateHeader();
+            _restClient.BaseUrl = new Uri(url);
+            _restClient.Timeout = -1;
         }
 
         /// GET pr_project
         /// https://docs.bexio.com/#tag/Projects
-        public async Task<IEnumerable<Project>> GetProjectsAsync() {
-            var response = await GetAsync("pr_project");
-            IEnumerable<Project> list = JsonConvert.DeserializeObject<IEnumerable<Project>>(response);
+        public List<Project> GetProjects(string orderBy = "id", int offset = 0, int limit = 1000) {
+            var request = new RestRequest($"pr_project?order_by={orderBy}&offset={offset}&limit={limit}", Method.GET);
+            IRestResponse response = ExecuteRestRequestWithBearer(request);
+            List<Project> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Project>>(response.Content);
             return list;
         }
 
-        /// GET timesheet
+       /// GET timesheet
         /// https://docs.bexio.com/#tag/Timesheet
-        public async Task<IEnumerable<Timesheet>> GetTimesheetsAsync() {
-            var response = await GetAsync("timesheet");
-            IEnumerable<Timesheet> list = JsonConvert.DeserializeObject<IEnumerable<Timesheet>>(response);
+        public List<Timesheet> GetTimesheets(string orderBy = "id", int offset = 0, int limit = 1000) {
+            var request = new RestRequest($"timesheet?order_by={orderBy}&offset={offset}&limit={limit}", Method.GET);
+            IRestResponse response = ExecuteRestRequestWithBearer(request);
+            List<Timesheet> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Timesheet>>(response.Content);
             return list;
         }
-
+ 
+        /// POST timesheet/search
+        /// https://docs.bexio.com/#operation/v2SearchTimesheets
+        public List<Timesheet> SearchTimesheets(List<TimesheetSearchBody> data)
+        {
+            var request = new RestRequest($"timesheet/search", Method.POST);
+            string jsonString;
+            jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            request.AddParameter("application/json", jsonString,  ParameterType.RequestBody);
+            IRestResponse response = ExecuteRestRequestWithBearer(request);
+            List<Timesheet> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Timesheet>>(response.Content);
+            return list;
+        }
+/*
         /// POST timesheet
         /// https://docs.bexio.com/#operation/v2CreateTimesheet
         public async Task<Timesheet> SaveTimesheetAsync(Timesheet data)
@@ -51,7 +66,50 @@ namespace bexio.net
             var response = await PostAsync("timesheet", ToDictionary(data));
             Timesheet sheet = JsonConvert.DeserializeObject<Timesheet>(response);
             return sheet;
+        }*/
+
+
+        #region Helpers
+
+        private void AddHeaders(RestRequest request) {
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
         }
+
+        private void AddBearer(RestRequest request, string accessToken = null) {
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Authorization", "Bearer " + accessToken);
+        }
+
+        private IRestResponse ExecuteRestRequestWithBearer(RestRequest request, int trial = 0)
+        {
+            AddBearer(request, _apiToken);
+            IRestResponse response = _restClient.Execute(request);
+
+            if (response.ErrorException == null)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized) {
+                    if (trial <= 3) {
+                        response = ExecuteRestRequestWithBearer(request, ++trial);
+                    } else {
+                        throw new Exception("Internal REST API call experienced too many trials.");
+                    }
+                } else if (response.StatusCode == System.Net.HttpStatusCode.NotFound) {
+                    throw new Exception("Internal REST API call could not connect to endpoint.");
+                }
+            } else {
+                throw new Exception("Internal REST API call experienced an issue.");
+            }
+            return response;
+        }
+
+        #endregion
+
+
+
+
+
+
+
 
         private AuthenticationHeaderValue GenerateHeader() {
             return new AuthenticationHeaderValue("Bearer", _apiToken);
