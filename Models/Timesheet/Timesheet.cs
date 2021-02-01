d6c3188b3c482984ed776295d4795ed66e9964a2
miko866 <m.durik@substring.ch>
2021-02-01 22:09:00 +0100

    Adds models attributes for timesheets

--- Models/Timesheet/Timesheet.cs
+++ Models/Timesheet/Timesheet.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-
 namespace bexio.net.Models
 {
 	public class Timesheet : TimesheetBase
 	{
 		public int id { get; set; }
-		// public string travel_time { get; set; }
-		// public string travel_charge { get; set; }
-		// public string travel_distance { get; set; }
-		// public string date { get; set; }
-		// public string duration { get; set; }
-		// public Boolean running { get; set; }
+		public string date { get; set; }
+		public string duration { get; set; }
 	}
 }
