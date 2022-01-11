cd2e1fbecc2f1b4b7083faaeccafbc5a621fccc5
ntold <n.berchtold@substring.ch>
2022-01-11 14:38:06 +0100

    fixes bug with null-values for JsonSerializer

--- bexio.net/Models/Other/User/FictionalUser.cs
+++ bexio.net/Models/Other/User/FictionalUser.cs
@@ -7,6 +7,6 @@ namespace bexio.net.Models.Other.User
         public string Firstname      { get; set; } = "";
         public string Lastname       { get; set; } = "";
         public string Email          { get; set; } = "";
-        public int    TitleId        { get; set; }
+        public int?   TitleId        { get; set; }
     }
 }
